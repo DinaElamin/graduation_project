16de1df2ef19e793baf1faa7101fafc8db534957
ThanaaAdel <swaadl971@gmail.com>
2024-04-17 20:43:42 +0200

    get all teacher without photo

---
+++ lib/features/manager/feature_home_manager_page/logic/cubits/get_all_student_cubit/login_cubit/get_all_student_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:ablexa/features/manager/feature_home_manager_page/data/repos/get_all_teacher_repo/get_all_teacher_repo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'get_all_teacher_state.dart';
+
+class GetAllTeacherDataCubit extends Cubit<GetAllTeacherDataState> {
+  final GetAllTeacherRepo getAllTeacherRepo;
+  GetAllTeacherDataCubit(this.getAllTeacherRepo) : super(const GetAllTeacherDataState.initial());
+
+  void emitAllTeacherStates() async {
+    emit(const GetAllTeacherDataState.loading());
+    final response = await getAllTeacherRepo.getAllTeacherData();
+
+    response.when(
+      success: (loginResponse) {
+        emit(GetAllTeacherDataState.success(loginResponse));
+      },
+      failure: (error) {
+        emit(GetAllTeacherDataState.error(error: error.apiErrorModel.title ?? ''));
+      },
+    );
+  }
+
+}
